f003bfd5e66defe6e29292b1c425d73418c638ad
Chandrashekhar <shekhar.kotekar@gmail.com>
2023-08-05 18:24:10 +0200

    python+flask calculator created

--- python_calculator/src/main.py
+++ python_calculator/src/main.py
@@ -1,1 +1,37 @@
-print("Hello calculator")
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/health')
+def health():
+    return f"Alive at {datetime.now()}"
+
+
+@app.route('/add')
+def add():
+    num1: int = int(request.args.get('num1'))
+    num2: int = int(request.args.get('num2'))
+    return f"{num1} + {num2} = {num1 + num2}"
+
+
+@app.route('/subtract')
+def subtract():
+    num1: int = int(request.args.get('num1'))
+    num2: int = int(request.args.get('num2'))
+    return f"{num1} - {num2} = {num1 - num2}"
+
+
+@app.route('/multiply')
+def multiply():
+    num1: int = int(request.args.get('num1'))
+    num2: int = int(request.args.get('num2'))
+    return f"{num1} * {num2} = {num1 * num2}"
+
+
+@app.route('/divide')
+def divide():
+    num1: int = int(request.args.get('num1'))
+    num2: int = int(request.args.get('num2'))
+    return f"{num1} / {num2} = {num1 / num2}"
