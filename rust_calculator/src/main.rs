fdd83b501e6188aade86d530ab4c50409d433b6a
Chandrashekhar <shekhar.kotekar@gmail.com>
2023-08-05 11:21:41 +0200

    rust calculator micro service is dockerized

--- rust_calculator/src/main.rs
+++ rust_calculator/src/main.rs
@@ -1,16 +1,16 @@
 use actix_web::{App, get, HttpResponse, HttpServer, Responder};
 
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
+#[get("/health")]
+async fn health() -> impl Responder {
     HttpResponse::Ok().body("I'm alive!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let ip_address: &str = "127.0.0.1";
+    let ip_address: &str = "0.0.0.0";
     let port_number: u16 = 8080;
-    println!("Starting server at http://{}:{}", ip_address, port_number);
+    println!("Starting calculator server at http://{}:{}", ip_address, port_number);
     HttpServer::new(|| {
-        App::new().service(health_check)
+        App::new().service(health)
     }).bind((ip_address, port_number))?.run().await
 }
