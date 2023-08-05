eef8aaf570169881aeb836200c548ef11bf3cfe7
Chandrashekhar <shekhar.kotekar@gmail.com>
2023-08-05 11:46:52 +0200

    Now rust calculator supports adding two numbers

--- README.md
+++ README.md
@@ -13,7 +13,12 @@ Repository contains code for calculator micro services developed using Python an
 - Execute `docker run -p 8080:8080 rust_calculator:latest` to run the server in local
 
 ### Docker image size evolution:
-- 5 August 2023 - Initial image size with distroless image: 28.8 MB. This image contains only health check service implemented.
+- 5 August 2023 - After adding `add()` function and `serde` dependency.
+```commandline
+REPOSITORY       TAG      IMAGE ID       SIZE
+rust_calculator  latest   2aac2868b938   28.9MB
+```
+- 5 August 2023 - Initial image size with distroless image. This image contains only health check service implemented.
 ```commandline
 REPOSITORY       TAG     IMAGE ID       SIZE
 rust_calculator  latest  efcf1758c197   28.8MB
--- rust_calculator/src/main.rs
+++ rust_calculator/src/main.rs
@@ -1,9 +1,23 @@
-use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Numbers {
+    first: i32,
+    second: i32,
+}
 
 #[get("/health")]
 async fn health() -> impl Responder {
     HttpResponse::Ok().body("I'm alive!")
 }
+
+#[get("/add")]
+async fn add(numbers: web::Query<Numbers>) -> impl Responder {
+    let sum = numbers.first + numbers.second;
+    HttpResponse::Ok().body(format!("{} + {} = {}", numbers.first, numbers.second, sum))
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,6 +25,6 @@ async fn main() -> std::io::Result<()> {
     let port_number: u16 = 8080;
     println!("Starting calculator server at http://{}:{}", ip_address, port_number);
     HttpServer::new(|| {
-        App::new().service(health)
+        App::new().service(health).service(add)
     }).bind((ip_address, port_number))?.run().await
 }
