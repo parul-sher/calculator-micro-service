e35a35dcd8dbc01df1a33a2123c8d10259d1a827
Chandrashekhar <shekhar.kotekar@gmail.com>
2023-08-05 10:48:40 +0200

    health check implemented on rust calculator

--- rust_calculator/src/main.rs
+++ rust_calculator/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("I'm alive!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let ip_address: &str = "127.0.0.1";
+    let port_number: u16 = 8080;
+    println!("Starting server at http://{}:{}", ip_address, port_number);
+    HttpServer::new(|| {
+        App::new().service(health_check)
+    }).bind((ip_address, port_number))?.run().await
 }
